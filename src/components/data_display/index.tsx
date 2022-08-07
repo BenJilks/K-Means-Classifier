eacc30e9e333a337af474f7716e4fda6ecce3dfe
BenJilks <benjyjilks@gmail.com>
2022-08-07 17:50:03 +0100

    Basic grid display.
    A simple grid pattern you can drag around using the mouse.

--- src/components/data_display/index.tsx
+++ src/components/data_display/index.tsx
@@ -1,18 +1,63 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './index.module.css'
+
+const grid_size_px = 100
+const grid_color = '#999'
 
 export default function DataDisplay() {
     const canvas_ref = useRef(document.createElement('canvas'))
+    const [offset, set_offset] = useState({ x: 0, y: 0 })
+    const [is_mouse_down, set_is_mouse_down] = useState(false)
+
     useEffect(() => {
         const canvas = canvas_ref.current
         const context = canvas.getContext('2d')!
-        context.fillStyle = '#000'
+        canvas.width = canvas.getBoundingClientRect().width
+        canvas.height = canvas.getBoundingClientRect().height
+
+        context.fillStyle = '#FFF'
         context.fillRect(0, 0, canvas.width, canvas.height)
-    })
+
+        const start_x = offset.x % grid_size_px
+        for (let x = 0; x < (canvas.width / grid_size_px) + 1; x++) {
+            context.lineWidth = 1
+            context.strokeStyle = grid_color
+            context.moveTo(x * grid_size_px + start_x, 0)
+            context.lineTo(x * grid_size_px + start_x, canvas.height)
+            context.stroke()
+        }
+
+        const start_y = offset.y % grid_size_px
+        for (let y = 0; y < (canvas.height / grid_size_px) + 1; y++) {
+            context.lineWidth = 1
+            context.strokeStyle = grid_color
+            context.moveTo(0, y * grid_size_px + start_y)
+            context.lineTo(canvas.width, y * grid_size_px + start_y)
+            context.stroke()
+        }
+    }, [offset])
+
+    const on_mouse_down = () => set_is_mouse_down(true)
+    const on_mouse_up = () => set_is_mouse_down(false)
+    const on_mouse_move = (event: React.MouseEvent) => {
+        if (!is_mouse_down)
+            return
+
+        set_offset({
+            x: offset.x + event.movementX,
+            y: offset.y + event.movementY,
+        })
+    }
 
     return (
         <div className={ styles.data_display }>
-            <canvas ref={ canvas_ref } className={ styles.canvas }></canvas>
+            <canvas
+                ref={ canvas_ref }
+                className={ styles.canvas }
+                onMouseDown={ on_mouse_down }
+                onMouseUp={ on_mouse_up }
+                onMouseMove={ on_mouse_move }>
+            </canvas>
         </div>
     )
 }
