de10495d862089017e3e1b416df020d44ba5d2d2
BenJilks <benjyjilks@gmail.com>
2022-08-09 13:35:30 +0100

    Only stroke once per frame, instead of per line.
    This improves drawing performance.

--- src/components/data_display/index.tsx
+++ src/components/data_display/index.tsx
@@ -15,26 +15,27 @@ export default function DataDisplay() {
         canvas.width = canvas.getBoundingClientRect().width
         canvas.height = canvas.getBoundingClientRect().height
 
+        // TODO: Replace this with just drawing textures,
+        //       to improve performance.
+
         context.fillStyle = '#FFF'
         context.fillRect(0, 0, canvas.width, canvas.height)
+        context.lineWidth = 1
+        context.strokeStyle = grid_color
 
         const start_x = offset.x % grid_size_px
         for (let x = 0; x < (canvas.width / grid_size_px) + 1; x++) {
-            context.lineWidth = 1
-            context.strokeStyle = grid_color
             context.moveTo(x * grid_size_px + start_x, 0)
             context.lineTo(x * grid_size_px + start_x, canvas.height)
-            context.stroke()
         }
 
         const start_y = offset.y % grid_size_px
         for (let y = 0; y < (canvas.height / grid_size_px) + 1; y++) {
-            context.lineWidth = 1
-            context.strokeStyle = grid_color
             context.moveTo(0, y * grid_size_px + start_y)
             context.lineTo(canvas.width, y * grid_size_px + start_y)
-            context.stroke()
         }
+
+        context.stroke()
     }, [offset])
 
     const on_mouse_down = () => set_is_mouse_down(true)
